b9f84109412d91be7ad0cfca453baf697b3e9cb5
Dave Ashman <me@iamdash.com>
2012-05-24 14:11:35 +0100

    Fixed project links
    Signed-off-by: Dave Ashman <me@iamdash.com>

--- includes/content/project-thumbs.php
+++ includes/content/project-thumbs.php
@@ -2,17 +2,17 @@
     <ul>
         <li class="alpha">
             <a href="/projects/ltm-mtm" title="LTM Exhibiton 'Mind the Map'">
-            <img src="/images/projects/ltm-mtm-thumb.png"  alt="" class="cms php-framework exhibition overlay" rel="/projects/ltm-mtm" />
+            <img src="/images/projects/ltm-mtm-thumb.png"  alt="" class="cms php-framework exhibition overlay" rel="/projects/ltm-mtm.php" />
             </a>
         </li>        
         <li>
             <a href="/projects/doolittles" title="Doolittles Dispensary" >
-                <img src="/images/projects/doolittles-thumb.png"  alt="doolittlesdispensary.com" class="frontend cms php-framework ecommerce overlay" rel="/projects/doolittles" />
+                <img src="/images/projects/doolittles-thumb.png"  alt="doolittlesdispensary.com" class="frontend cms php-framework ecommerce overlay" rel="/projects/doolittles.php" />
             </a>
         </li>
         <li class="omega">
             <a href="/projects/visa-esta" title="ESTA VISA" >
-            <img src="/images/projects/visaesta-thumb.png"  alt="visa-esta-usa.com" class="frontend cms php-framework ecommerce overlay" rel="/projects/visa-esta"  />
+            <img src="/images/projects/visaesta-thumb.png"  alt="visa-esta-usa.com" class="frontend cms php-framework ecommerce overlay" rel="/projects/visa-esta.php"  />
             </a>
         </li>
         <?php /*
@@ -23,33 +23,33 @@
         </li>  */?>
         <li class="alpha">
             <a href="/projects/nrm" title="National Railway Museum">
-            <img src="/images/projects/nrm-thumb.png"  alt="nrm.org.uk" class="frontend overlay"  rel="/projects/nrm"  />
+            <img src="/images/projects/nrm-thumb.png"  alt="nrm.org.uk" class="frontend overlay"  rel="/projects/nrm.php"  />
             </a>
         </li>        
         <li>
             <a href="/projects/snog" title="Snog Frozen Yogurt">
-            <img src="/images/projects/snog-thumb.png"  alt="ifancyasnog.com" class="frontend cms wordpress overlay" rel="/projects/snog" />
+            <img src="/images/projects/snog-thumb.png"  alt="ifancyasnog.com" class="frontend cms wordpress overlay" rel="/projects/snog.php" />
             </a>
         </li>
         <li class="omega">
             <a href="/projects/ltm-sense" title="LTM Exhibiton 'Sense in the City'">
-            <img src="/images/projects/ltm-sense-thumb.png"  alt="" class="cms php-framework exhibition overlay" rel="/projects/ltm-sense" />
+            <img src="/images/projects/ltm-sense-thumb.png"  alt="" class="cms php-framework exhibition overlay" rel="/projects/ltm-sense.php" />
             </a>
         </li>
         <li class="alpha">
             <a href="/projects/yoobi" title="Yoobi">
-            <img src="/images/projects/yoobi-thumb.png"  alt="loveyoobi.com" class="cms frontend wordpress overlay" rel="/projects/yoobi" />
+            <img src="/images/projects/yoobi-thumb.png"  alt="loveyoobi.com" class="cms frontend wordpress overlay" rel="/projects/yoobi.php" />
             </a>
         </li> 
 
         <li>
             <a href="/projects/vinspired" title="Vinspired Voicebox">
-            <img src="/images/projects/voicebox-thumb.png"  alt="voicebox.vinspired.com" class="cms frontend php-framework exhibition overlay" rel="/projects/vinspired"   />
+            <img src="/images/projects/voicebox-thumb.png"  alt="voicebox.vinspired.com" class="cms frontend php-framework exhibition overlay" rel="/projects/vinspired.php"   />
             </a>
         </li>
         <li class="omega">
             <a href="/projects/hallagenna" title="Hallagenna Riding Stables">
-            <img src="/images/projects/hallagenna-thumb.png"  alt="doolittlesdispensary.com" class="cms frontend joomla overlay" rel="/projects/hallagenna"   />
+            <img src="/images/projects/hallagenna-thumb.png"  alt="doolittlesdispensary.com" class="cms frontend joomla overlay" rel="/projects/hallagenna.php"   />
             </a>
         </li>     
     </ul>
