55e73e7051b610275373f19ba0b055b8706b5cc5
Dave Ashman <public@iamdash.com>
2012-06-21 14:04:16 +0100

    Adding tag v.01

---
+++ includes/js/libs/jquery.inview-ck.js
@@ -0,0 +1,4 @@
+/**
+ * author Remy Sharp
+ * url http://remysharp.com/2009/01/26/element-in-view-event-plugin/
+ */(function(a){function b(){var b=window.innerHeight,c=document.compatMode;if(c||!a.support.boxModel)b=c=="CSS1Compat"?document.documentElement.clientHeight:document.body.clientHeight;return b}a(window).scroll(function(){var c=b(),d=document.documentElement.scrollTop?document.documentElement.scrollTop:document.body.scrollTop,e=[];a.each(a.cache,function(){this.events&&this.events.inview&&e.push(this.handle.elem)});e.length&&a(e).each(function(){var b=a(this),e=b.offset().top,f=b.height(),g=b.data("inview")||!1;if(d>e+f||d+c<e){if(g){b.data("inview",!1);b.trigger("inview",[!1])}}else if(d<e+f&&!g){b.data("inview",!0);b.trigger("inview",[!0])}})});a(function(){a(window).scroll()})})(jQuery);
