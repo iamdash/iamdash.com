2d7b7c4c4f592f91e699ef17163c3e4fbe4749cf
Dave Ashman <public@iamdash.com>
2012-10-20 20:39:16 +0100

    Added config file, functionality, but added to .gitignore

--- lib/fns.php
+++ lib/fns.php
@@ -1,14 +1,14 @@
 <?php
+require_once 'config.php';
 require_once 'phpflickr/phpFlickr.php';
 require_once 'image_cacher.php';
 
 function getFlickrImage(){
-	
 	$api_key 	= '8e04f641eb2354aa4de3333bf1ece363';
 	$secret 	= '49f92ed2cd7a9a93';
 	
 	$f = new phpFlickr($api_key,$secret);
-	//$f->enableCache("db", "mysql://iamdash_db:@87.239.18.178/iamdash_main");
+	$f->enableCache("db", 'mysql://'.DB_USER.':'.DB_PASS.'@'.DB_HOST.'/'.DB_NAME);
 
 	$photos = $f->people_getPublicPhotos ('53572347@N02',null,'url_o','original_format');
 	//d($photos['photos']['photo'][0]);exit();
