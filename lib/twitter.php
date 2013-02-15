5f94e79fc5043207f062aa46a0cb39c56303b64e
Dave Ashman [Music] <public@iamdash.com>
2013-02-15 09:48:55 +0000

    Commit of DB dump

--- lib/twitter.php
+++ lib/twitter.php
@@ -1,43 +1,43 @@
 <?php
 
 class Twitter{
-    
+
     protected $username;
     protected $tweetcount;
     var $cache_dir;
     var $expire_time=0;
-  
-    
+
+
     public function __construct($username,$tweetcount){
-        
+
         $this->cache_dir    = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'cache/twitter/tweets';
         //$this->expire_time  = $time_expire = time() + 24*60*60;
         //$this->expire_time  = 15*60;
-        
+
         make_cache_path($this->cache_dir);
-        
+
         $this->username = $username;
         $this->tweetcount = $tweetcount;
     }
-    
+
     /**
-     * 
+     *
      * @return type
      */
     public function getTweets(){
             $twitter_feed =  curl_download('http://api.twitter.com/1/statuses/user_timeline.json?count='.$this->tweetcount.'&screen_name='.$this->username);
             $tweets = json_decode($twitter_feed,false);
 
-            
+
             $tweets = $this->_cacheTweets($tweets);
 
             return $tweets;
             exit();
     }
 
-    
+
     /**
-     * 
+     *
      * @desc    Checks if tweet cache file exists. If not, we write the file, and return it's contents
      * @param type json file contents of cached file
      * @return type
@@ -56,24 +56,24 @@ class Twitter{
             return $this->_decodeTweets(file_get_contents($cache_file));
         }
     }
-    
+
     private function _decodeTweets($data){
         $data = mb_convert_encoding($data,'UTF-8');
         $data = preg_replace('/\\\u([0-9a-z]{4})/', '&#x$1;', $data);
-        
+
         return $this->_parseTweets(json_decode($data,true));
     }
-    
-    private function _parseTweets($tweets) {
-        
+
+    private function _parseTweets($tweet) {
+
         $i=0;
-        foreach($tweets as $tweet){
-            
+        #foreach($tweets as $tweet){
+
             $t = $tweet;
 
             // link URLs
             $t['text_parsed'] = " ".preg_replace( "/(([[:alnum:]]+:\/\/)|www\.)([^[:space:]]*)([[:alnum:]#?\/&=])/i", "<a href=\"\\1\\3\\4\" target=\"_blank\">\\1\\3\\4</a>", $t['text']);
-            
+
             // link mailtos
             $t['text_parsed']= preg_replace( "/(([a-z0-9_]|\\-|\\.)+@([^[:space:]]*)".
                     "([[:alnum:]-]))/i", "<a href=\"mailto:\\1\">\\1</a>", $t['text_parsed']);
@@ -88,13 +88,13 @@ class Twitter{
             $t['text_parsed'] = preg_replace("/>(([[:alnum:]]+:\/\/)|www\.)([^[:space:]]".
                     "{30,40})([^[:space:]]*)([^[:space:]]{10,20})([[:alnum:]#?\/&=])".
                     "</", ">\\3...\\5\\6<", $t['text_parsed']);
-            $parsed_tweets[$i]['count']     =   $i;
-            $parsed_tweets[$i]['created']   = strtotime($t['created_at']);
-            $parsed_tweets[$i]['tweet']     = preg_replace('/%u([a-fA-F0-9]{4})/', '&#x\\1;', $t['text_parsed']);
-            $parsed_tweets[$i]['username']  = $this->username;
+            #$parsed_tweets['count']     =   $i;
+            #$parsed_tweets['created']   = strtotime($t['created_at']);
+            #$parsed_tweets['tweet']     = preg_replace('/%u([a-fA-F0-9]{4})/', '&#x\\1;', $t['text_parsed']);
+            #$parsed_tweets['username']  = $this->username;
             $i++;
-        }
+        #}
 
-        return $parsed_tweets;
-    }    
+        return $t['text_parsed'];
+    }
 }
