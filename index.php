bbc46b763840655a9fa11d0e24996e766541380a
Dave Ashman <public@iamdash.com>
2013-06-24 21:49:30 +0100

    Fixed mobile layout

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $image = getFlickrImage();
 <html>
     <head>
         <title>iamdash.com</title>
+         <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0" />
         <meta charset="UTF-8" />
         <meta name="description" content="" />
         <link href="/favicon.ico" type="image/x-icon" rel="icon" />
