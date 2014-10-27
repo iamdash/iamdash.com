e47928e91545a4bfedaa71db821244f1ea343759
Dave Ashman <me@iamdash.com>
2014-10-27 18:33:50 +0000

    Commit before working on new site

---
+++ assets/scripts/src/background.js
@@ -0,0 +1,54 @@
+//var backgrounds = [
+//    {
+
+//        'image': {
+//            'file': 'autumn.jpg',
+//            'url': 'http://www.flickr.com/photos/iamdashnet/8103852614/in/photostream/',
+//            'title': 'Lovely, just lovely'
+//        },
+//        'image': {
+//            'file': 'feet.jpg',
+//            'url': 'http://www.flickr.com/photos/iamdashnet/8103852614/in/photostream/',
+//            'title': 'Lovely, just lovely'
+//        },
+//        'image': {
+//            'file': 'lake.jpg',
+//            'url': 'http://www.flickr.com/photos/iamdashnet/8103852614/in/photostream/',
+//            'title': 'Lovely, just lovely'
+//        },
+//        'image': {
+//            'file': 'landscape.jpg',
+//            'url': 'http://www.flickr.com/photos/iamdashnet/8103852614/in/photostream/',
+//            'title': 'Lovely, just lovely'
+//        },
+//        'image': {
+//            'file': 'pillars.jpg',
+//            'url': 'http://www.flickr.com/photos/iamdashnet/8103852614/in/photostream/',
+//            'title': 'Lovely, just lovely'
+//        },
+//        'image': {
+//            'file': 'van.jpg',
+//            'url': 'http://www.flickr.com/photos/iamdashnet/8103852614/in/photostream/',
+//            'title': 'Lovely, just lovely'
+//
+//        }
+//    }
+//];
+var backgrounds = [
+    'autumn.jpg',
+    'feet.jpg',
+    'lake.jpg',
+    'landscape.jpg',
+    'pillars.jpg',
+    'van.jpg'
+];
+
+//
+var bg = _(backgrounds).pickRandom();
+
+
+window.onload = function () {
+
+    $('#dash').backstretch('../assets/images/backgrounds/' + bg);
+//    $('a#photo-link').attr('href', bg.image.url)
+};
